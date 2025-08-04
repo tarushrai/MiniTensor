93edf9831378789acbf781341501cce568ef646c
Tarush Rai <160582142+tarushrai@users.noreply.github.com>
2025-08-04 11:25:01 +0530

    Update tensor.h

--- include/tensor.h
+++ include/tensor.h
@@ -9,6 +9,9 @@ private:
     std::vector<std::vector<float>> data;
 
 public:
+    Tensor add(const Tensor& other) const;
+Tensor multiply(const Tensor& other) const;
+float dot(const Tensor& other) const;
     Tensor(int rows, int cols);
     void set_value(int i, int j, float value);
     float get_value(int i, int j) const;
